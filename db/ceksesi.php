78d4255c5351eaf0b3e44be46c1cf633d9744e8a
MrSpcecial <Mettawijayawu@gmail.com>
2025-03-08 15:05:45 +0700

    Website WNS

--- db/ceksesi.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if(!isset($_SESSION['namauser'])) 
-    {
-    echo "<script>alert('Maaf Akses Ditolak Anda Belum Login..!!'); javascript:history.go(-1)</script>";
-    include_once "index.php";
-    exit;
-    }
-?>
