b63d2bff889d99a1355ea7cc30aa56d77be0ca40
MrSpcecial <Mettawijayawu@gmail.com>
2025-03-08 14:15:17 +0700

    Website WNS

--- js/script.js
+++ js/script.js
@@ -20,15 +20,17 @@ document.getElementById("search-icon").addEventListener("click", function (e) {
   searchBox.classList.toggle("hidden"); // Toggle kelas hidden untuk tampil/sembunyi
 });
 
-document.querySelectorAll(".menu-card").forEach(function (card) {
-  card.addEventListener("click", function () {
-    // Show the order box and the "Bayar" button
-    const orderBox = card.querySelector(".order-box");
-    const payButton = document.getElementById("pay-btn");
-    orderBox.style.display = "block";
-    payButton.style.display = "block";
-  });
-});
+// document.querySelectorAll(".menu-card").forEach(function (card) {
+//   card.addEventListener("click", function () {
+//     // Show the order box and the "Bayar" button
+//     const orderBox = card.querySelector(".order-box");
+//     const payButton = document.getElementById("pay-btn");
+//     const totaltext = document.getElementById("overall-total");
+//     orderBox.style.display = "block";
+//     payButton.style.display = "block";
+//     totaltext.style.display = "block";
+//   });
+// });
 
 document.querySelectorAll(".minus-btn").forEach(function (minusBtn) {
   minusBtn.addEventListener("click", function () {
@@ -49,40 +51,21 @@ document.querySelectorAll(".plus-btn").forEach(function (plusBtn) {
   });
 });
 
-// Initialize prices for items
-const prices = {
-  item1: 20000, // IDR 20K
-  item2: 15000, // IDR 15K
-};
-
-// Initialize total to 0
-let total = 0;
-
 // Update the total price for each item and overall total
 function updateTotal(button) {
-  const orderBox = button.closest(".order-box");
-  const quantity = orderBox.querySelector("input").value;
-  const price = orderBox
-    .closest(".menu-card")
-    .querySelector(".menu-card-price").textContent;
-  const priceValue = parseInt(price.replace("IDR ", "").replace("K", ""));
-
-  // Update the individual total
-  const totalElement = orderBox.querySelector("span");
-  totalElement.textContent = priceValue * quantity + "K";
-
-  // Update the overall total
   let overallTotal = 0;
+
   document.querySelectorAll(".order-box").forEach(function (box) {
-    const boxQuantity = box.querySelector("input").value;
-    const boxPrice = box
+    const quantity = box.querySelector("input").value;
+    const price = box
       .closest(".menu-card")
       .querySelector(".menu-card-price").textContent;
-    const boxPriceValue = parseInt(
-      boxPrice.replace("IDR ", "").replace("K", "")
-    );
-    overallTotal += boxQuantity * boxPriceValue;
+    const priceValue = parseInt(price.replace("IDR ", "").replace("K", "000"));
+
+    overallTotal += quantity * priceValue;
   });
+
+  // Update overall total
   document.getElementById("overall-total").textContent =
-    "IDR " + overallTotal + "K";
+    "IDR " + overallTotal.toLocaleString() + ",00";
 }
